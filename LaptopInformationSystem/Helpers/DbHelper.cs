c3d7d40b5363f88627ecf06c21420a26a3988bf9
sushan-ratna-tuladhar <sushanrt@gmail.com>
2021-11-21 18:20:07 +0545

    Added laptop details adding code

--- LaptopInformationSystem/Helpers/DbHelper.cs
+++ LaptopInformationSystem/Helpers/DbHelper.cs
@@ -1,9 +1,8 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LaptopInformationSystem.Helpers
 {
@@ -38,13 +37,61 @@ namespace LaptopInformationSystem.Helpers
                 Console.WriteLine("Updating AccessDate");
                 cmd.ExecuteReader();
                 Console.WriteLine("AccessDate Updated");
-                this.conn.Close();
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Error on UpdateAccessedDate : " + ex.Message + ", stack: " + ex.StackTrace);
             }
+            finally
+            {
+                if (this.conn.State == ConnectionState.Open)
+                {
+                    this.conn.Close();
+                }
+            }
             
+        }
+
+        public string AddDevice(string code, string model, string type, string purchasedOn)
+        {
+            try
+            {
+                this.GetDbConnection();
+
+                string addDeviceCommand = "INSERT INTO devices(code, model, type, purchased_on) VALUES (@code, @model, @type, @purchasedOn);";
+
+                MySqlCommand cmd = this.conn.CreateCommand();
+                cmd.CommandText = addDeviceCommand;
+                cmd.Parameters.AddWithValue("@code", code);
+                cmd.Parameters.AddWithValue("@model", model);
+                cmd.Parameters.AddWithValue("@type", type);
+                cmd.Parameters.AddWithValue("@purchasedOn", purchasedOn);
+                Console.WriteLine("Adding Device, code: " + code + ", model: " + model + ", type: " + type + ", purchasedOn: " + purchasedOn.ToString());
+                this.conn.Open();
+                cmd.ExecuteReader();
+                this.conn.Close();
+                Console.WriteLine("Device Added, code: " + code + ", model: " + model + ", type: " + type + ", purchasedOn: " + purchasedOn.ToString());
+                return ("Device : " + code + " Added!!!");
+            }
+            catch (Exception ex)
+            {
+                if(ex.Message.Contains("Duplicate"))
+                {
+                    Console.WriteLine(ex.Message);
+                    return ("Duplicate code!!!");
+                }
+
+                Console.WriteLine("Error on AddDevice : " + ex.Message + ", stack: " + ex.StackTrace);
+                return ("Something went wrong");
+            }
+            finally
+            {
+                if(this.conn.State == ConnectionState.Open)
+                {
+                    this.conn.Close();
+                }
+            }
+
         }
 
     }
